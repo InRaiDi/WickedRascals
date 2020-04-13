6d0903db9eab6f71a73d7b4aec89573918dbc6b7
Samuli Lehtonen <samulilehtonen33@gmail.com>
2020-04-14 01:57:09 +0300

    Character movement edit + started enemy path movement
    Enemy path movement is still under development

--- JungleMaze/Assets/Scripts/EnemyMovement.cs
+++ JungleMaze/Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    //Ememies
+    public GameObject bird1;
+    public GameObject bird2;
+    public GameObject bird3;
+    public GameObject snail1;
+    public GameObject snail2;
+    public GameObject snail3;
+
+    float birdMovementSpeed = 15.0f;
+    float snailMovementSpeed = 10.0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,27 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bird1.transform.position.Set(-40.0f, 21.0f, 6.787284f);
+        //bird2.transform.Translate(birdMovementSpeed * Time.deltaTime, 0, 0);
+        //bird3.transform.Translate(birdMovementSpeed * Time.deltaTime, 0, 0);
+        //snail1.transform.Translate(snailMovementSpeed * Time.deltaTime, 0, 0);
+        //snail2.transform.Translate(snailMovementSpeed * Time.deltaTime, 0, 0);
+        //snail3.transform.Translate(snailMovementSpeed * Time.deltaTime, 0, 0);
+        if (bird1.transform.position.x >= 11 && bird1.transform.position.y >= 21) { }
+        {
+            bird1.transform.Translate(0, -birdMovementSpeed * Time.deltaTime, 0);
+        }
+        if (bird1.transform.position.x >= 11 && bird1.transform.position.y <= 20) { }
+        {
+            bird1.transform.Translate(-birdMovementSpeed * Time.deltaTime, 0, 0);
+        }
+        if (bird1.transform.position.x <= -40 && bird1.transform.position.y <= 20) { }
+        {
+            bird1.transform.Translate(0, birdMovementSpeed * Time.deltaTime, 0);
+        }
+        if (bird1.transform.position.x >= -40 && bird1.transform.position.y >= 21) { }
+        {
+            bird1.transform.Translate(birdMovementSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
--- JungleMaze/Assets/Scripts/MonkeyMovement.cs
+++ JungleMaze/Assets/Scripts/MonkeyMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,7 @@ public class MonkeyMovement : MonoBehaviour
 
     //speed
     float movementSpeed = 10.0f;
+    bool rotation = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,26 +27,36 @@ public class MonkeyMovement : MonoBehaviour
     //Check different inputs during screen update
     void actionByInput()
     {
-
+        //Move up
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0, movementSpeed * Time.deltaTime, 0);
         }
-
+        //Move down
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(0, -movementSpeed * Time.deltaTime, 0);
         }
-
+        //Move left
         if (Input.GetKey(KeyCode.A))
         {
-
-            transform.Translate(-movementSpeed * Time.deltaTime, 0, 0);
+            //check if not rotated, rotate
+            if (!rotation)
+            {
+                transform.Rotate(0, 180.0f, 0.0f, Space.Self);
+                rotation = true;
+            }
+            transform.Translate(movementSpeed * Time.deltaTime, 0, 0);
         }
-
+        //Move right
         if (Input.GetKey(KeyCode.D))
         {
-
+            //check if rotated, unrotate
+            if (rotation)
+            {
+                transform.Rotate(0, 180.0f, 0.0f, Space.Self);
+                rotation = false;
+            }
             transform.Translate(movementSpeed * Time.deltaTime, 0, 0);
         }
 
