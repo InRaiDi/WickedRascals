4d78d5c9097738ed5e6c780f986fb6bf797e9be4
Samuli Lehtonen <samulilehtonen33@gmail.com>
2020-04-16 14:21:08 +0300

    lever, doors, death and respawn
    Added more functions and movement + minor edits

--- JungleMaze/Assets/Scripts/OpenDoor.cs
+++ JungleMaze/Assets/Scripts/OpenDoor.cs
@@ -5,22 +5,74 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
 
+    int rotateSpeed = 1;
 
+    //door rotation
+    bool rotateDoor1 = true;
+    bool rotateDoor2 = true;
+    //check if lever isn't pressed yet
+    bool lever1 = false;
+    bool lever2 = false;
+
+    GameObject door1;
+    GameObject door2;
+
+    void Start()
+    {
+      door1 = GameObject.Find("Door1");
+      door2 = GameObject.Find("Door2");
+    }
+
+    void Update()
+    {
+        if (lever1) {
+
+            if (rotateDoor1)
+            {
+                door1.transform.Rotate(0, 0, rotateSpeed, Space.Self);
+            }
+            if(door1.transform.rotation.z >= 0.7f)
+            {
+                rotateDoor1 = false;
+            }
+        }
+
+        if (lever2)
+        {
+            if (rotateDoor2)
+            {
+                door2.transform.Rotate(0, 0, rotateSpeed, Space.Self);
+            }
+            if (door2.transform.rotation.z >= 0.7f)
+            {
+                rotateDoor2 = false;
+            }
+            
+        }
+
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Lever1"))
         {
-            var door1 = GameObject.Find("Door1");
-            door1.transform.position = new Vector2(-23.24f, 3.08f);
-        
+            lever1 = true;
+            SpriteRenderer sr = other.GetComponent<SpriteRenderer>();
+            sr.flipY = true;
+            //var door1 = GameObject.Find("Door1");
+            ////door1.transform.position = new Vector2(-23.24f, 3.08f);
+
+
         }
 
         if (other.gameObject.CompareTag("Lever2"))
         
         {
-          var door2 = GameObject.Find("Door2");
-          door2.transform.position = new Vector2(26.86f, -27.79f);
+            lever2 = true;
+            SpriteRenderer sr = other.GetComponent<SpriteRenderer>();
+            sr.flipY = true;
+            //var door2 = GameObject.Find("Door2");
+            //door2.transform.position = new Vector2(26.86f, -27.79f);
 
         }
     }
--- JungleMaze/Assets/Scripts/ScoreManager.cs
+++ JungleMaze/Assets/Scripts/ScoreManager.cs
@@ -7,7 +7,9 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public TextMeshProUGUI text;
+    public Transform spawnPoint;
     int score;
+    int health = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,17 @@ public class ScoreManager : MonoBehaviour
         Debug.Log(score);
         score += coinValue;
         text.text = "X" + score.ToString();
+    }
+
+    public void decreaseHealth()
+    {
+        health--;
+
+        if(health <= 0)
+        {
+            GameObject monkey = GameObject.Find("Sylvester");
+            monkey.transform.position = spawnPoint.position;
+        }
     }
  
 
